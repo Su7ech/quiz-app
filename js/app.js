574e54129f7ed8db4f712c96b0ead80a075bb240
Su7ech <tpbaroody@gmail.com>
2015-08-27 11:11:28 -0700

    Started javascript code

--- js/app.js
+++ js/app.js
@@ -1,3 +1,27 @@
 $(document).ready(function() {
 	
 });
+
+// function Question(question, choices, correctAnswer) {
+// 	this.question = question;
+// 	this.choices = choices;
+// 	this.answer = correctAnswer;
+// 	this.userAnswer = "";
+// }
+
+// Question.prototype.getCorrectAnswer = function() {
+// 	return this.answer;
+// }
+
+// Question.prototype.getUserAnswer = function() {
+// 	return this.userAnswer;
+// }
+
+// Question.prototype.displayQuestion = function() {
+// 	var questionToDisplay = "<div class='quiz-question'>" + this.question + "</div><ul>";
+// 	choiceCounter = 0;
+
+// 	this.choices.forEach(function(eachChoice) {
+// 		questionToDisplay += '<li>'
+// 	})
+// }
